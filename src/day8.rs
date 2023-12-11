6ef2a19f593610b1c263bbfc0e40e369c7a47c6b
Joe Sorensen <hello@murl.digital>
2023-12-11 12:51:53 -0700

    precalculate ROOT_2GH

--- src/day8.rs
+++ src/day8.rs
@@ -1,13 +1,8 @@
 use actix_web::{get, web};
-use lazy_static::lazy_static;
 use rustemon::client::RustemonClient;
 
-// yes i could just put this in the equation directly, but if it wasnt for the SQRT THIS COULD JUST BE A CONST
-// LIKE WTF RUST THIS WAS AN ISSUE IN 2019, IT IS 2023
-// eat shit and die
-lazy_static! {
-    static ref ROOT_2GH: f64 = (2.0f64 * 9.825f64 * 10.0f64).sqrt();
-}
+// (2.0f64 * 9.825f64 * 10.0f64).sqrt()
+const ROOT_2GH: f64 = 14.0178457689;
 
 
 #[get("/8/weight/{idx}")]
@@ -23,5 +18,5 @@ pub async fn vaporeon_splat(client: web::Data<RustemonClient>, idx: web::Path<i6
 
     let weight = mon.weight as f64 / 10_f64;
 
-    (*ROOT_2GH * weight).to_string()
+    (ROOT_2GH * weight).to_string()
 }
